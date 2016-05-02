ddeaa48027b9a97ac7d15219368fd461eaad38a0
ClikeX <w.s.van.der.meulen@gmail.com>
2016-05-02 17:51:27 +0200

    Use Ruby's OpenSSL library

--- lib/spoiled.rb
+++ lib/spoiled.rb
@@ -1,20 +1,32 @@
+require 'socket'
+require 'openssl'
+
 require "active_support/all"
 
 require "spoiled/version"
 
 module Spoiled
   def self.verify url, port = 443
-    command = "echo | openssl s_client -connect #{url}:#{port} 2>/dev/null | openssl x509 -noout -dates"
-    output = `#{command}`
+    tcp_client = TCPSocket.new(url, 443)
+    ssl_client = OpenSSL::SSL::SSLSocket.new(tcp_client)
+    ssl_client.connect
+    cert = OpenSSL::X509::Certificate.new(ssl_client.peer_cert)
+    ssl_client.sysclose
+    tcp_client.close
 
-    not_before = output.lines.first.to_date
-    not_after = output.lines.last.to_date
-    time_remaining = ( not_after - Date.today ).to_i.days
+    certprops = OpenSSL::X509::Name.new(cert.issuer).to_a
+    issuer = certprops.select { |name, data, type| name == "O" }.first[1]
 
-    {
-      not_before: not_before,
-      not_after: not_after,
-      time_remaining: time_remaining
+    cn = cert.subject.to_s.gsub!(/.*?(?=CN=)/im, "").gsub("CN=", '')
+
+    result = {
+      url: url,
+      port: port,
+      valid_on: cert.not_before.to_date,
+      valid_until: cert.not_after.to_date,
+      issuer: issuer,
+      time_remaining: ( cert.not_after.to_date - Date.today ).to_i.days,
+      common_name: cn
     }
   end
 end
